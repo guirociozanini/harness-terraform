9c91d2804a5db6e3e5945208553e2e79dfaccd07
guilherme zanini <guilherme.zanini@harness.io>
2022-09-09 11:49:10 -0300

    ecs instance role

--- exercise_5/main.tf
+++ exercise_5/main.tf
@@ -44,7 +44,7 @@ resource "aws_launch_configuration" "ecs-launch-configuration-1" {
 
   security_groups             = "${var.ecs_security_groups}"
   associate_public_ip_address = "true"
-  key_name                    = "harness-delegate"
+  key_name                    = "guiherme-zanini"
 
   user_data = <<EOF
 #!/bin/bash
