681e6716186a76cb846dea2546a118a38371f6be
guilherme zanini <guilherme.zanini@harness.io>
2022-09-09 11:44:18 -0300

    ecs instance role

--- exercise_5/main.tf
+++ exercise_5/main.tf
@@ -31,7 +31,7 @@ resource "aws_launch_configuration" "ecs-launch-configuration-1" {
   name                 = "ecs-lb-${var.ecs_cluster_1}"
   image_id             = "ami-0693a7971cd761811"
   instance_type        = "t2.medium"
-  iam_instance_profile = "ECS-role"
+  iam_instance_profile = "arn:aws:iam::014474954170:role/ECS-role"
 
   root_block_device {
     volume_type           = "standard"
